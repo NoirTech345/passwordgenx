5dde612aec469a56aa072475860455e96e1f547f
NoirTech345 <103590217+NoirTech345@users.noreply.github.com>
2024-04-07 22:26:31 +0300

    Update passwordgenx_code.py

--- passwordgenx_code.py
+++ passwordgenx_code.py
@@ -1,11 +1,14 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, Toplevel, Listbox
 import random
 import string
 import pyperclip
-from PIL import Image, ImageTk
-import pystray
+import json
+import os
 
+
+
+password_history = []
 messages = {
     'en': {
         'length_prompt': "Enter the desired length of the password:",
@@ -16,7 +19,14 @@ messages = {
         'generate_password': "Generate Password",
         'password_length_note': "Password length should be between 6 and 45 characters.",
         'select_language': "Select Language",
-        'minimize_to_tray': "Minimize to Tray"
+        'minimize_to_tray': "Minimize to Tray",
+        'copy_warning': "Do you really want to copy the password?",
+        'show_password_history': "Show Password History",
+        'password_not_copied': "Password was not copied",
+        'clear_all': "Clear all",
+        'delete': "Delete",
+        'copy': "Copy",
+
     },
     'ua': {
         'length_prompt': "Введіть бажану довжину пароля:",
@@ -27,7 +37,14 @@ messages = {
         'generate_password': "Згенерувати пароль",
         'password_length_note': "Довжина пароля повинна бути від 6 до 45 символів.",
         'select_language': "Виберіть мову",
-        'minimize_to_tray': "Згорнути в трей"
+        'minimize_to_tray': "Згорнути в трей",
+        'copy_warning': "Ви дійсно хочете скопіювати пароль?",
+        'show_password_history': "Показати історію паролів",
+        'password_not_copied': "Пароль не було скопійовано",
+        'clear_all': "Очистити все",
+        'delete': "Видалити",
+        'copy': "Копіювати",
+
     },
     'ru': {
         'length_prompt': "Введите желаемую длину пароля:",
@@ -38,7 +55,13 @@ messages = {
         'generate_password': "Сгенерировать пароль",
         'password_length_note': "Длина пароля должна быть от 6 до 45 символов.",
         'select_language': "Выберите язык",
-        'minimize_to_tray': "Свернуть в трей"
+        'minimize_to_tray': "Свернуть в трей",
+        'copy_warning': "Вы действительно хотите скопировать пароль?",
+        'show_password_history': "Показать историю паролей",
+        'password_not_copied': "Пароль не был скопирован",
+        'clear_all': "Очистить все",
+        'delete': "Удалить",
+        'copy': "Копировать",
     },
     'by': {
         'length_prompt': "Увядзіце жаданую даўжыню пароля:",
@@ -49,7 +72,14 @@ messages = {
         'generate_password': "Згенераваць пароль",
         'password_length_note': "Даўжыня пароля павінна быць ад 6 да 45 знакаў.",
         'select_language': "Выберыце мову",
-        'minimize_to_tray': "Згарнуць у трэй"
+        'minimize_to_tray': "Згарнуць у трэй",
+        'copy_warning': "Вы сапраўды хочаце скапіяваць пароль?",
+        'show_password_history': "Паказаць гісторыю пароляў",
+        'password_not_copied': "Пароль не быў скапіяваны",
+        'clear_all': "Ачысціць усё",
+        'delete': "Выдаліць",
+        'copy': "Капіяваць",
+
     },
     'kz': {
         'length_prompt': "Пароль ұзындығын көрсетіңіз:",
@@ -60,7 +90,13 @@ messages = {
         'generate_password': "Парольді жасау",
         'password_length_note': "Парольдың ұзындығы 6-ден 45 таңбадан аз болмауы керек.",
         'select_language': "Тілді таңдау",
-        'minimize_to_tray': "Лақтырғышқа түсіру"
+        'minimize_to_tray': "Лақтырғышқа түсіру",
+        'copy_warning': "Сіз шынымен парольді көшірмекші ме?",
+        'show_password_history': "Құпия сөз тарихын көрсету",
+        'password_not_copied': "Құпия сөз көшірілмеді",
+        'clear_all': "Барлығын Тазалаңыз",
+        'delete': "Жою",
+        'copy': "Көшіру",
     },
     'pl': {
         'length_prompt': "Podaj żądaną długość hasła:",
@@ -71,9 +107,34 @@ messages = {
         'generate_password': "Generuj hasło",
         'password_length_note': "Długość hasła powinna wynosić od 6 do 45 znaków.",
         'select_language': "Wybierz język",
-        'minimize_to_tray': "Minimalizuj do zasobnika"
+        'minimize_to_tray': "Minimalizuj do zasobnika",
+        'copy_warning': "Czy na pewno chcesz skopiować hasło?",
+        'show_password_history': "Pokaż historię haseł",
+        'password_not_copied': "Hasło nie zostało skopiowane",
+        'clear_all': "Wyczyść wszystko",
+        'delete': "Usuwać",
+        'copy': "Kopiuj",
     }
 }
+
+
+#hi?
+
+
+def save_password_history():
+    os.makedirs(os.path.expanduser('~/Мои Документы/passwordgenxfiles'), exist_ok=True)
+
+    with open(os.path.expanduser('~/Мои Документы/passwordgenxfiles/password_history.json'), 'w') as f:
+        json.dump(password_history, f)
+
+def load_password_history():
+    try:
+        with open(os.path.expanduser('~/Мои Документы/passwordgenxfiles/password_history.json'), 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+    
+password_history = load_password_history()
 
 def generate_password(length):
     if length < 6 or length > 45:
@@ -83,6 +144,19 @@ def generate_password(length):
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
+
+
+
+
+def delete_selected_password(listbox):
+    selected_index = listbox.curselection()[0]
+    del password_history[selected_index]
+    save_password_history()
+    listbox.delete(selected_index)
+
+
+
+
 def generate_and_display_password():
     try:
         length = int(entry_length.get())
@@ -90,11 +164,47 @@ def generate_and_display_password():
             raise ValueError(messages[language]['password_length_note'])
         password = generate_password(length)
         if password:
+            password_history.append(password)
+            save_password_history()
+            update_password_history()
             label_generated.config(text=messages[language]['generated_password'] + password)
-            pyperclip.copy(password)
-            label_copied.config(text=messages[language]['copied_to_clipboard'])
+            if messagebox.askyesno("Предупреждение", messages[language]['copy_warning']):
+                pyperclip.copy(password)
+                label_copied.config(text=messages[language]['copied_to_clipboard'])
+                root.after(5000, lambda: label_copied.config(text=""))  
+            else:
+                label_copied.config(text=messages[language]['password_not_copied'])
+                root.after(5000, lambda: label_copied.config(text="")) 
     except ValueError as e:
         messagebox.showerror("Error", str(e))
+
+
+def show_password_history():
+    history_window = Toplevel(root)
+    history_window.title("Password History")
+    history_window.geometry("320x380")  
+    history_window.resizable(True, True) 
+    history_listbox = Listbox(history_window)
+    history_listbox.pack(fill='both', expand=True)
+    for password in password_history:
+        history_listbox.insert(tk.END, password)
+    copy_button = tk.Button(history_window, text=messages[language]['copy'], command=lambda: pyperclip.copy(history_listbox.get(history_listbox.curselection())))
+    copy_button.pack(fill='both', expand=True)
+    delete_button = tk.Button(history_window, text=messages[language]['delete'], command=lambda: delete_selected_password(history_listbox))
+    delete_button.pack(fill='both', expand=True)
+    clear_button = tk.Button(history_window, text=messages[language]['clear_all'], command=lambda: clear_password_history(history_listbox))
+    clear_button.pack(fill='both', expand=True)
+
+def clear_password_history(listbox):
+    password_history.clear()
+    save_password_history()  
+    listbox.delete(0, tk.END)
+
+def update_password_history():
+    if 'history_listbox' in globals():
+        history_listbox.delete(0, END)
+        for password in password_history:
+            history_listbox.insert(END, password)
 
 def change_language():
     global language
@@ -104,12 +214,6 @@ def change_language():
         messagebox.showinfo(messages[language]['change_language'], f"{messages[language].get('language_changed', 'Language changed')}: {language.upper()}.")
         update_interface(language)
 
-def minimize_to_tray():
-    root.withdraw()
-    icon = pystray.Icon("")
-    icon.icon = Image.open("icon.png")
-    icon.visible = True
-    icon.run()
 
 def update_interface(language):
     label_length.config(text=messages[language]['length_prompt'])
@@ -118,48 +222,36 @@ def update_interface(language):
     label_copied.config(text="")
     password_note_label.config(text=messages[language]['password_length_note'])
     button_language.config(text=messages[language]['change_language'])
-    button_minimize.config(text=messages[language]['minimize_to_tray'])
+    button_history.config(text=messages[language]['show_password_history'])
+
 
 root = tk.Tk()
 root.title("PasswordGenX (created by noirtech)")
 root.geometry("400x250")
-
 label_length = tk.Label(root, text=messages['en']['length_prompt'])
 label_length.pack(fill='both', expand=True)
-
 entry_length = tk.Entry(root)
 entry_length.pack(fill='both', expand=True)
-
 button_generate = tk.Button(root, text=messages['en']['generate_password'], command=generate_and_display_password)
 button_generate.pack(fill='both', expand=True)
-
 label_generated = tk.Label(root, text="")
 label_generated.pack(fill='both', expand=True)
-
 label_copied = tk.Label(root, text="")
 label_copied.pack(fill='both', expand=True)
-
 variable_language = tk.StringVar(root)
 variable_language.set('en')
 option_menu = tk.OptionMenu(root, variable_language, 'en', 'ua', 'ru', 'by', 'kz', 'pl')
 option_menu.pack(fill='both', expand=True)
-
 button_language = tk.Button(root, text=messages['en']['change_language'], command=change_language, font=("Arial", 10))
 button_language.pack(fill='both', expand=True)
-
-button_minimize = tk.Button(root, text=messages['en']['minimize_to_tray'], command=minimize_to_tray, font=("Arial", 10))
-button_minimize.pack(fill='both', expand=True)
-
+button_history = tk.Button(root, text=messages['ru']['show_password_history'], command=show_password_history)
+button_history.pack(fill='both', expand=True)
 password_note_label = tk.Label(root, text=messages['en']['password_length_note'], fg="red")
 password_note_label.pack(fill='both', expand=True)
-
 language = 'en'
 update_interface(language)
 
 root.mainloop()
-
-
-
 
 
 
